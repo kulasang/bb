6de5fcc56104a7e61961f22e9ee9b93f26d01cf7
kulasang <kulasang@yahoo.com>
2018-03-23 22:17:58 +0700

    uuu
    uuu

--- LINETCR/Api/channel.py
+++ LINETCR/Api/channel.py
@@ -18,8 +18,8 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Line/7.14.0"
-    LA = "IOSIPAD\t7.14.0\tiPhone OS\t10.12.0"
+    UA = "Line/8.3.2"
+    LA = "IOSIPAD\t8.3.2\tiPhone OS\t10.12.0"
 
     authToken = None
     mid = None
